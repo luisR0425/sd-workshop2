99231b6e87ca5280da3642c23a137bfc7aed827f
Luis <luisf0425@gmail.com>
2018-02-14 19:40:33 -0500

    receive_Sned

--- A00315320/send.py
+++ A00315320/send.py
@@ -8,12 +8,8 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
-
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Holi')
+print(" [x] Sent 'Hello World!'")
+connection.close()
